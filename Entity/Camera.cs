03b5779a86d629a8eb117a04f6872a47f8562fb9
Metodija Naumov <mnaumov@hotmail.com>
2025-07-29 15:42:53 +0200

    Created Entity project, created Camera class

---
+++ Entity/Camera.cs
@@ -0,0 +1,11 @@
+﻿namespace Entity
+{
+    public class Camera
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public double Latitude { get; set; }
+
+        public double Longitude { get; set; }
+    }
+}
