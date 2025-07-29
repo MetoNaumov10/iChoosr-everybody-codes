4ed8ddfc3f9e3ebe464d27f21de8c691594fdd68
Metodija Naumov <mnaumov@hotmail.com>
2025-07-29 15:52:57 +0200

    Created Services project. Added ICameraService interface and CameraService class. They will be used in the CLI app and the API for getting the cameras from the .csv file via dependency injection

---
+++ Services/CameraService.cs
@@ -0,0 +1,35 @@
+﻿using Entity;
+
+namespace Services
+{
+    public class CameraService : ICameraService
+    {
+       public List<Camera> LoadCameras()
+        {
+            List<Camera> cameras = new List<Camera>();
+
+            var lines = File.ReadAllLines(@"..\data\cameras-defb.csv").Skip(1);
+
+            foreach (var line in lines)
+            {
+                if (string.IsNullOrWhiteSpace(line)) continue;
+
+                var parts = line.Split(';');
+
+                if (parts.Length != 3) continue;
+
+                if (double.TryParse(parts[1], out double lat) && double.TryParse(parts[2], out double lng))
+                {
+                    cameras.Add(new Camera
+                    {
+                        Name = parts[0].Trim(),
+                        Latitude = lat,
+                        Longitude = lng
+                    });
+                }
+            }
+
+            return cameras;
+        }
+    }
+}
